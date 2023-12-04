73883d8c41b969c5e2a669280ddb617dfd358ba1
manojbajaj <manoj.bajaj@gmail.com>
2023-12-04 23:15:02 +0000

    Day1- refactoring

--- src/test/java/com/bajajma/aoc2023/PuzzleTest.java
+++ src/test/java/com/bajajma/aoc2023/PuzzleTest.java
@@ -7,23 +7,29 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PuzzleTest {
 
     private Calibrator calibrator = new Calibrator();
     @Test
-    void puzzle1() throws Exception {
+    void day1Puzzle1() throws Exception {
         Path path = Paths.get(getClass().getClassLoader().getResource("puzzle1-input.txt").toURI());
         Stream<String> lines = Files.lines(path);
        // lines.map(line -> calibrator.calibrationValue(line)).forEach(value -> System.out.println(value));
-        System.out.println(lines.map(line -> calibrator.calibrationValue(line)).reduce(0, Integer::sum));
+      //  System.out.println(lines.map(line -> calibrator.calibrationValue(line)).reduce(0, Integer::sum));
+        assertThat(lines.map(line -> calibrator.calibrationValue(line)).reduce(0, Integer::sum)).isEqualTo(55607);
     }
 
     @Test
-    void puzzle2() throws Exception {
+    void day1Puzzle2() throws Exception {
         Path path = Paths.get(getClass().getClassLoader().getResource("puzzle1-input.txt").toURI());
         Stream<String> lines = Files.lines(path);
       //lines.map(line -> calibrator.calibrationValueWithLetters(line)).forEach(value -> System.out.println(value));
-       System.out.println(lines.map(line -> calibrator.calibrationValueWithLetters(line)).reduce(0, Integer::sum));
+       //System.out.println(lines.map(line -> calibrator.calibrationValueWithLetters(line)).reduce(0, Integer::sum));
+
+        assertThat(lines.map(line -> calibrator.calibrationValueWithLetters(line)).reduce(0, Integer::sum)).isEqualTo(55291);
+
     }
 
 }
